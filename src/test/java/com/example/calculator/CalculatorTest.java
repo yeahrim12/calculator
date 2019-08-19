6b81bd4b99022ec014de3cd2782afa6e257a446d
Yelim Jeon <ojkjkjk@naver.com>
2019-08-19 10:43:21 +0900

    뺄셈에 대한 테스트케이스 데이터 수정

--- src/test/java/com/example/calculator/CalculatorTest.java
+++ src/test/java/com/example/calculator/CalculatorTest.java
@@ -54,14 +54,14 @@ public class CalculatorTest {
     @Test
     public void test_양수뺄셈() {
         Calculator calc = new Calculator();
-        int result = calc.substract(5,4);
-        assertEquals("두 뺄셈의 차는 1이어야 합니다.",1, result);
+        int result = calc.substract(20,10);
+        assertEquals("두 뺄셈의 차는 10이어야 합니다.",10, result);
     }
 
     @Test
     public void test_양수곱셈() {
         Calculator calc = new Calculator();
-        int result = calc.multiply(5,4);
-        assertEquals("두 곱셈의 값은 20이어야 합니다.",20, result);
+        int result = calc.multiply(4,4);
+        assertEquals("두 곱셈의 값은 16이어야 합니다.",16, result);
     }
 }
